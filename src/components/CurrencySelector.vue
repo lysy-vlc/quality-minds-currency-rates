4a2032d112501412bbcf0c29e57ddc052f19789c
lysy-vlc <kamil.grzaba@gmail.com>
2022-03-05 02:32:25 +0100

    adds currency selector

---
+++ src/components/CurrencySelector.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="currency-selector">
+    <select name="currency-selector" @change="handleChange">
+      <option value="">Select currency</option>
+      <option
+          v-for="currency in props.currencies"
+          :key="currency.code"
+          :value="currency.code"
+      >{{ currency.currency }} ({{ currency.code }})</option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  currencies: {
+    type: Array,
+    required: true,
+    default: () => []
+  }
+})
+
+const emit = defineEmits(['change'])
+
+const handleChange = (event) => {
+  emit('change', event.target.value)
+}
+</script>
+
+<style scoped lang="scss">
+.currency-selector {
+
+  margin-bottom: 20px;
+
+  select {
+    padding: 10px;
+  }
+}
+</style>
