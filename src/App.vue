93b665d2d29b2e147ab5e1d5569a49f288a72cb6
lysy-vlc <kamil.grzaba@gmail.com>
2022-03-05 02:33:13 +0100

    composes inouts and selectors, adds application logic

--- src/App.vue
+++ src/App.vue
@@ -1,125 +1,142 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+// components
+import CurrencySelector from '@/components/CurrencySelector.vue'
+import Input from '@/components/Input.vue'
+import Loader from '@/components/Loader.vue'
+
+import { onMounted, ref } from 'vue'
+
+// constants
+const TABLE = 'A'
+
+// refs
+const currencyTable = ref([])
+const selectedCurrency = ref(null)
+const inputValues = ref({
+  first: null,
+  second: null
+})
+const loading = ref(false)
+
+// methods
+const getCurrencyTable = async () => {
+  try {
+    const response = await fetch(`https://api.nbp.pl/api/exchangerates/tables/${TABLE}`)
+
+    const data = await response.json()
+
+    currencyTable.value = data[0]
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const getCurrentCurrencyRate = async (code, table) => {
+  try {
+    loading.value = true
+
+    const result = await fetch(`https://api.nbp.pl/api/exchangerates/rates/${table}/${code}/`)
+
+    const data = await result.json()
+
+    return data?.rates[0]?.mid
+  } catch (e) {
+    console.error(e)
+  } finally {
+    loading.value = false
+  }
+}
+
+const handleSelectedCurrencyChange = (value) => {
+  inputValues.value = {
+    first: null,
+    second: null
+  }
+
+  selectedCurrency.value = value
+}
+
+const debounce = (func, timeout = 400) => {
+  let timer
+
+  return (...args) => {
+    clearTimeout(timer)
+
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, timeout)
+  }
+}
+
+const handleInputChange = async (value, name) => {
+  if (value) {
+    const rate = await getCurrentCurrencyRate(selectedCurrency.value, TABLE)
+
+    if (name == 'first') {
+      inputValues.value = {
+        first: value,
+        second: value / rate
+      }
+
+      return
+    }
+
+    inputValues.value = {
+      first: value * rate,
+      second: value
+    }
+
+    return
+  }
+
+  inputValues.value = {
+    first: null,
+    second: null
+  }
+}
+
+const debounceInputChange = debounce((value, name) => handleInputChange(value, name))
+
+// lifecycle methods
+onMounted(() => {
+  getCurrencyTable()
+})
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
+  <main>
+    <CurrencySelector
+        @change="handleSelectedCurrencyChange"
+        :currencies="currencyTable.rates"
     />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="inputs-container" v-if="selectedCurrency">
+      <Input
+          @change="debounceInputChange"
+          name="first"
+          currency="PLN"
+          :values="inputValues"
+          :disabled="loading"
+      />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <Loader v-if="loading"/>
+
+      <Input
+          @change="debounceInputChange"
+          name="second"
+          :currency="selectedCurrency"
+          :values="inputValues"
+          :disabled="loading"
+      />
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
 
-<style>
-@import '@/assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style scoped>
+.inputs-container {
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 </style>
