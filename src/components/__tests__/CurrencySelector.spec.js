86eafbde015f6261fb61ea160872a6351f9f267e
lysy-vlc <kamil.grzaba@gmail.com>
2022-03-08 15:25:15 +0100

    tests CurrencySelector component

---
+++ src/components/__tests__/CurrencySelector.spec.js
@@ -0,0 +1,43 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import CurrencySelector from '../CurrencySelector.vue'
+
+const currencies = [
+  {
+    'currency': 'bat (Tajlandia)',
+    'code': 'THB',
+    'mid': 0.136
+  },
+  {
+    'currency': 'dolar amerykański',
+    'code': 'USD',
+    'mid': 4.5115
+  }
+]
+
+describe('Input', () => {
+  it('renders properly', async () => {
+    const wrapper = mount(CurrencySelector, {
+      props: {
+        currencies
+      }
+    })
+
+    await wrapper.find('select').setValue(currencies[0].code)
+
+    expect(wrapper.find('select').text()).toContain('THB')
+  })
+
+  it('emits select event properly', async () => {
+    const wrapper = mount(CurrencySelector, {
+      props: {
+        currencies
+      }
+    })
+
+    await wrapper.find('select').setValue(currencies[0].code)
+
+    expect(wrapper.emitted().change).toEqual([ [ currencies[0].code ] ])
+  })
+})
